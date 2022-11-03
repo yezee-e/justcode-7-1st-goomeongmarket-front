b803219e1ee85f6960bf399ee3de24ea6acd9933
yeji <yejipooh@gmail.com>
2022-11-03 15:08:01 +0900

    merge 전 저장

--- src/components/Category/Dropdown.jsx
+++ src/components/Category/Dropdown.jsx
@@ -1,10 +1,15 @@
+//각 필터별 mapping하기
+//각 카테고리 flip state만들기
+//FaAngleDown아이콘 keyfram 효과넣기
+
+//filter 넣고 ->다시 필터빼기 //map,useEffect()
 import React from 'react';
 import { useState } from 'react';
 import { FaAngleDown } from 'react-icons/fa';
 
 function Dropdown({ data, setData, list }) {
   const [isActive, setIsActive] = useState(false);
-  const [isSelect, setIsSelect] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
   //   console.log(data[0].category);
 
   //카테고리 필터기능
@@ -12,6 +17,7 @@ function Dropdown({ data, setData, list }) {
     let result = data.filter(fakeData => fakeData.category == item);
     setData(result);
   };
+
   return (
     <div className="dropdown">
       <div className="dropdown-btn" onClick={e => setIsActive(!isActive)}>
