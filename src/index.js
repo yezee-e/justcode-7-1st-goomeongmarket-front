6e8b3268e30e7b04f354a517fc2fe0b2e2dd7992
오수빈 <catherine621@naver.com>
2022-11-05 22:24:28 +0900

    conflict 해결1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import Router from './pages/Router';
-import './styles/reset.scss';
-import './styles/common.scss';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -20,4 +9,3 @@ root.render(
     <Router />
   </React.StrictMode>
 );
->>>>>>> 69aa1b7a0983c37e3394c90f210c5a27cc5aca73
